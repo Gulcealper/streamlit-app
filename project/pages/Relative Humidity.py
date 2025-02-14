8804e7a0fac12ecf7f3a5e1d57863660f411cd56
Gulce Alper <47519683+Gulcealper@users.noreply.github.com>
2025-02-14 12:19:41 +0000

    Update Relative Humidity.py

--- project/pages/Relative Humidity.py
+++ project/pages/Relative Humidity.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
-from streamlit_extras.app_logo import add_logo
 import plotly.graph_objects as go
 import seaborn as sns
 
