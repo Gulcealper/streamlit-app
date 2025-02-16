147f6ca46652ee15552e90ef72b71618e0ff71d3
Gulce Alper <47519683+Gulcealper@users.noreply.github.com>
2025-02-16 17:02:55 +0000

    Update Precipitation.py

--- project/pages/Precipitation.py
+++ project/pages/Precipitation.py
@@ -14,13 +14,7 @@ st.title("Precipitation")
 
 
 
-@st.cache_data
-def load_data():
-    df = pd.read_parquet("project/new_df.parquet")
-    return df
 
-if 'data' not in st.session_state:
-    st.session_state.data = load_data()
 
 # Access the data from session state
 df = st.session_state.data
