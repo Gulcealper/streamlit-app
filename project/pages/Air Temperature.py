b7c835d09731aaf09530926c4cd3bbd0233f1a45
Gulce Alper <47519683+Gulcealper@users.noreply.github.com>
2025-02-17 22:25:17 +0000

    Update Air Temperature.py

--- project/pages/Air Temperature.py
+++ project/pages/Air Temperature.py
@@ -121,7 +121,9 @@ st.plotly_chart(fig, key="10")
 
 
 
-st.sidebar.title("Select for Seasonal View")
+st.sidebar.title("""
+    <h1 style='font-size: 24px; font-weight: bold;'>Select for Seasonal View</h1>
+""", unsafe_allow_html=True)
 selected_year = st.sidebar.selectbox("Select Year: (Optional)", [None] + list(df.index.year.unique()))
 if selected_year:
     st.header("Sesonal Averages")
